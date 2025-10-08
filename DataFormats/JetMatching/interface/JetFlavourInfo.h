48f8985d93b4574f931f04ab371c728d4aa27f6a
Chi Wang <chi.w@cern.ch>
2025-10-08 12:32:20 +0200

    :sparkles: Reserving slot for hadron-based GHS

--- DataFormats/JetMatching/interface/JetFlavourInfo.h
+++ DataFormats/JetMatching/interface/JetFlavourInfo.h
@@ -32,9 +32,9 @@ namespace reco {
  */
   /// typedef for
   /// enumeration class for the fastjet::contrib flavour definition algorithms
-  enum class FlavAlgo { kCMP, kGHS, kIFN, kGHSFull };
+  enum class FlavAlgo { kCMP, kGHS, kIFN, kGHSFull, kGHSInlineParton, kGHSInlineHadron };
   // Number of fastjet::contrib flavour definition algorithms
-  constexpr size_t kAlgoFlavCount = 4;
+  constexpr size_t kAlgoFlavCount = 6;
   constexpr size_t kAlgoArrayLen = 7;
 
   class JetFlavourInfo {
